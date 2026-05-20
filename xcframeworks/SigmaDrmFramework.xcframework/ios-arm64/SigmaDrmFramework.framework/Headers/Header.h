1ac78cefea22c6ada76b364020c02a362c1fffee
phamngochai123 <haipn32@wru.vn>
2026-05-21 05:50:00 +0700

    update modulemap

---
+++ xcframeworks/SigmaDrmFramework.xcframework/ios-arm64/SigmaDrmFramework.framework/Headers/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  SigmaDrmFramework
+//
+//  Created by Pham Hai on 21/5/26.
+//  Copyright © 2026 NguyenVanSao. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
