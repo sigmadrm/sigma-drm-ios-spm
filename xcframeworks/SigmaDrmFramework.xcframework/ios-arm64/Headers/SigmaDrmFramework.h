0f6558115cac1be34452596119be93fddec585f6
phamngochai123 <haipn32@wru.vn>
2026-05-21 09:36:53 +0700

    Refactor SigmaDrmFramework structure: replace framework files with static library and header files, update Info.plist paths, and add module map for Swift integration.

---
+++ xcframeworks/SigmaDrmFramework.xcframework/ios-arm64/Headers/SigmaDrmFramework.h
@@ -0,0 +1,13 @@
+//
+//  SigmaDrmFramework.h
+//  SigmaDrmFramework
+//
+
+#ifndef SigmaDrmFramework_h
+#define SigmaDrmFramework_h
+
+#import <Foundation/Foundation.h>
+
+#import "SigmaDRM.h"
+
+#endif /* SigmaDrmFramework_h */
