368b18554575806f328324b6e4c0e2022efaf105
phamngochai123 <haipn32@wru.vn>
2026-05-21 08:44:38 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,24 @@
-// swift-tools-version:5.10
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SigmaDrmFramework",
-    platforms: [
-        .iOS(.v12)
-    ],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "SigmaDrmFramework",
-            targets: ["SigmaDrmFramework"]
-        )
+            targets: ["SigmaDrmFramework"]),
     ],
+    dependencies: [],
     targets: [
         .binaryTarget(
             name: "SigmaDrmFramework",
-            path: "SigmaDrmFramework.xcframework"
-        )
+            path: "xcframeworks/SigmaDrmFramework.xcframework"
+        ),
+        .target(
+            name: "SigmaDrmFramework"
+        ),
     ]
 )
