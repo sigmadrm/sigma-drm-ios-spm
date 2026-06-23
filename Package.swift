56edb26243af4f7a905696709d5bcb019d9faa07
phamngochai123 <haipn32@wru.vn>
2026-06-23 17:18:09 +0700

    fix: change setting Package.swift

--- Package.swift
+++ Package.swift
@@ -4,22 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "SigmaDRM",
+    name: "SigmaDrmSPM",
     platforms: [.iOS(.v10)],
     products: [
         .library(
-            name: "SigmaDRM",
-            targets: ["SigmaDRM", "SigmaDrmFramework"]),
+            name: "SigmaDrmFramework",
+            targets: ["SigmaDRM", "SigmaDrmBinaryFramework"]),
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "SigmaDrmFramework",
+            name: "SigmaDrmBinaryFramework",
             path: "xcframeworks/SigmaDRM.xcframework"
         ),
         .target(
-            name: "SigmaDRM",
-            dependencies: ["SigmaDrmFramework"]
+            name: "SigmaDRM"
         ),
     ]
 )
--- Sources/SigmaDRM/Dummy.swift
+++ Sources/SigmaDRM/Dummy.swift
@@ -1,1 +1,2 @@
 import Foundation
+@_exported import SigmaDrmFramework
